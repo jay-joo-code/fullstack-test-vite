f5665d5ce0822556aeebbf652e9b6629643c0e7d
Jay Joo <jj534@cornell.edu>
2021-05-12 14:15:30 +0900

    Optimize bundle size

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,6 +16,25 @@ export default defineConfig({
   build: {
     rollupOptions: {
       plugins: [analyze(), visualize({ open: true })],
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('react')) {
+            return 'react'
+          }
+          if (id.includes('date')) {
+            return 'date'
+          }
+          if (id.includes('antd')) {
+            return 'antd'
+          }
+          if (id.includes('lodash')) {
+            return 'lodash'
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
     },
   },
 })
